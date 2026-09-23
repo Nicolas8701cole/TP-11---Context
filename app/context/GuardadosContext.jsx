1b9f4f9371af192fc668fe141d10873fb6defe61
Nicolas8701 <168933211+Nicolas8701@users.noreply.github.com>
2026-09-23 07:58:59 -0300

    aclaradoSiTerminadoONo

--- app/context/GuardadosContext.jsx
+++ app/context/GuardadosContext.jsx
@@ -1,4 +1,10 @@
 import { createContext, useState } from "react"
 export const GuardadosContext = createContext()
 
-export function GuardadosContext
+export function GuardadosContext({children}){//children es el contenido del prop
+const [guardados, setGuardados] = useState([])
+}
+export function EstaGuardado ({idPosteo}){ //Corroboro si id existe
+ return guardados.includes(idPosteo)
+}
+
